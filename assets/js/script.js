b742b09109e46213f26cfce11f9331e539015c3a
SafUmmSul91 <63952978+SafUmmSul91@users.noreply.github.com>
2024-05-24 22:13:39 +0100

    remove reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,63 @@
-const computerChoiceDsiplay = document.getElementById('computer-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
-const resultDesplay = document.getElementById('result');
+const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 
-let userChoice
+let userChoice;
+let computerChoice;
+let result;
+
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
-    
+    generateComputerChoice();
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1; // or possibleChoice.length
+    if(randomNumber === 1) {
+        computerChoice = 'rock';
+    }
+    if(randomNumber === 2) {
+        computerChoice = 'paper';
+    }
+    if(randomNumber === 3) {
+        computerChoice = 'scissors';
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+    getResult();
+}
+
+function getResult() {
+    if(computerChoice === userChoice) {
+        resultDisplay.innerHTML = "Draw!";
+    }
+    if(computerChoice === 'rock' && userChoice === 'paper') {
+        resultDisplay.innerHTML = "You win!";
+    }
+    if(computerChoice === 'rock' && userChoice === 'scissors') {
+        resultDisplay.innerHTML = "You lose!";
+    }
+    if(computerChoice === 'paper' && userChoice === 'rock') {
+        resultDisplay.innerHTML = "Oops!";
+    }
+    if(computerChoice === 'paper' && userChoice === 'scissors') {
+        resultDisplay.innerHTML = "You win!";
+    }
+    if(computerChoice === 'scissors' && userChoice === 'rock') {
+        resultDisplay.innerHTML = "You win!";
+    }
+    if(computerChoice === 'scissors' && userChoice === 'paper') {
+        resultDisplay.innerHTML = "Not quite, buddy!";
+    }
+};
+
+function resetGame() {
+    // Clear the result
+    resultDisplay.innerHTML = '';
+    // Clear user choice
+    userChoiceDisplay.innerHTML = '';
+    // Clear computer choice
+    computerChoiceDisplay.innerHTML = '';
+}
